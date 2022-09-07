a2dd44170ae6721e06beeae57a380034feddfb8d
Bhupender Singh <bhupenders225@gmail.com>
2022-09-08 02:04:44 +0530

    cleaned basic code

--- src/api/middleware/error.handler.js
+++ src/api/middleware/error.handler.js
@@ -1,8 +1,8 @@
 const httpStatus = require("http-status");
+const createError = require("http-errors");
 const logger = require("../../config/logger");
 
-// eslint-disable-next-line no-unused-vars
-const handler = (err, req, resp, next) => {
+const handler = (err, req, resp) => {
 	logger.error(`Err Handler: ${err.message}`);
 	return resp.status(err.status || 500).json({
 		code: err.status || 500,
@@ -11,9 +11,7 @@ const handler = (err, req, resp, next) => {
 };
 
 const notFound = (req, resp, next) => {
-	const error = new Error("Not Found");
-	error.status = 404;
-	next(error);
+	next(createError(404, "Route Not Available"));
 };
 
 module.exports = { handler, notFound };
